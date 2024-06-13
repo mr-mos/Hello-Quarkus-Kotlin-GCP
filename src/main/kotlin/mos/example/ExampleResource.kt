3d27da8185f2fc36c41b0e1d5f4f9229988f88f4
mos <mos.cheele+github@gmail.com>
2024-06-13 18:32:15 +0200

    Make "hello world" page root.

--- src/main/kotlin/mos/example/ExampleResource.kt
+++ src/main/kotlin/mos/example/ExampleResource.kt
@@ -5,10 +5,10 @@ import jakarta.ws.rs.Path
 import jakarta.ws.rs.Produces
 import jakarta.ws.rs.core.MediaType
 
-@Path("/hello")
+@Path("/")
 class ExampleResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    fun hello() = "Hello from Quarkus REST"
+    fun hello() = "Hello Quarkus, Kotlin and GCP. :)"
 }
