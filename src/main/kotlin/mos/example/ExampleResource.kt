f1bb6e3f7a04eabf1cde8daf4b350bb5cb563b11
mos <mos.cheele+github@gmail.com>
2024-06-27 18:35:57 +0200

    Update Quarkus Version 3.12.0  +  show version number in app

--- src/main/kotlin/mos/example/ExampleResource.kt
+++ src/main/kotlin/mos/example/ExampleResource.kt
@@ -4,11 +4,14 @@ import jakarta.ws.rs.GET
 import jakarta.ws.rs.Path
 import jakarta.ws.rs.Produces
 import jakarta.ws.rs.core.MediaType
+import org.eclipse.microprofile.config.inject.ConfigProperty
 
 @Path("/")
-class ExampleResource {
+class ExampleResource(
+    @ConfigProperty(name = "quarkus.used.version") val quarkusFrameworkVersion: String
+) {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    fun hello() = "Hello Quarkus, Kotlin and GCP. :))"
+    fun hello() = "Hello Quarkus ${quarkusFrameworkVersion}, Kotlin and GCP. :))"
 }
--- src/test/kotlin/mos/example/ExampleResourceTest.kt
+++ src/test/kotlin/mos/example/ExampleResourceTest.kt
@@ -2,19 +2,25 @@ package mos.example
 
 import io.quarkus.test.junit.QuarkusTest
 import io.restassured.RestAssured.given
+import org.eclipse.microprofile.config.ConfigProvider
+import org.eclipse.microprofile.config.inject.ConfigProperty
 import org.hamcrest.CoreMatchers.`is`
 import org.junit.jupiter.api.Test
 
+
 @QuarkusTest
-class ExampleResourceTest {
+class ExampleResourceTest  {
+
+
 
     @Test
     fun testHelloEndpoint() {
+        val quarkusFrameworkVersion =  ConfigProvider.getConfig().getConfigValue("quarkus.used.version").value
         given()
             .`when`().get("/")
             .then()
             .statusCode(200)
-            .body(`is`("Hello Quarkus, Kotlin and GCP. :))"))
+            .body(`is`("Hello Quarkus ${quarkusFrameworkVersion?:"NO VERSION"}, Kotlin and GCP. :))"))
     }
 
 }
