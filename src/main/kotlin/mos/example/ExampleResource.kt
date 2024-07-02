d0f0f203c040dbe1635388d0ee18af161e33e762
mos <mos.cheele+github@gmail.com>
2024-07-02 17:14:51 +0200

    Database:  Insert data dynamically via action; use sequence

--- src/main/kotlin/mos/example/ExampleResource.kt
+++ src/main/kotlin/mos/example/ExampleResource.kt
@@ -1,25 +1,49 @@
 package mos.example
 
 import io.quarkus.hibernate.orm.panache.kotlin.PanacheQuery
+import jakarta.transaction.Transactional
 import jakarta.ws.rs.GET
 import jakarta.ws.rs.Path
 import jakarta.ws.rs.Produces
 import jakarta.ws.rs.core.MediaType
+import jakarta.ws.rs.core.Response
 import org.eclipse.microprofile.config.inject.ConfigProperty
+import java.net.URI
+import java.util.Date
 
 @Path("/")
 class ExampleResource(
     @ConfigProperty(name = "quarkus.used.version") val quarkusFrameworkVersion: String
 ) {
 
+
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.TEXT_HTML)
     fun hello(): String {
         val all: List<MyDomain> = MyDomain.all().list()
-        return """Hello Quarkus ${quarkusFrameworkVersion}, Kotlin and GCP. :))
+        return """<pre>Hello Quarkus ${quarkusFrameworkVersion}, Kotlin and GCP. :))
             
 My database entries:
   ${all.joinToString("\n  ") { it.name }}
-            """
+   </pre>
+ <a href="insert">Add a row to DB table</a> 
+ 
+ <hr>
+ <small> Locally, check the MySQL Dev Service for the used port: <a href="http://localhost:8080/q/dev-ui/dev-services">dev-services</a>. 
+ """
     }
+
+
+    @GET
+    @Path("/insert")
+    @Transactional
+    fun insert(): Response {
+        val myDomain = MyDomain()
+        myDomain.name = "New ${Date()}"
+        myDomain.persist()
+        // return with redirect to hello()
+        return Response.seeOther(URI.create("/")).build()
+    }
+
+
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,5 @@
 insert into MyDomain values (1, 'test no 1');
 insert into MyDomain values (2, 'test no 2');
 insert into MyDomain values (3, 'test no 3');
+
+UPDATE MyDomain_SEQ SET next_val = 4;
