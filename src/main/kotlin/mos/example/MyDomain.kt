90200760dde464ee7540a50abd23ce4207a37f8c
mos <mos.cheele+github@gmail.com>
2024-07-02 17:35:42 +0200

    Database: Use MySQL AUTO_INCREMENT instead of sequence

--- src/main/kotlin/mos/example/MyDomain.kt
+++ src/main/kotlin/mos/example/MyDomain.kt
@@ -1,15 +1,23 @@
 package mos.example
 
 import io.quarkus.hibernate.orm.panache.kotlin.PanacheCompanion
-import io.quarkus.hibernate.orm.panache.kotlin.PanacheEntity
+import io.quarkus.hibernate.orm.panache.kotlin.PanacheEntityBase
 import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
 
 @Entity
-class MyDomain : PanacheEntity() {
+class MyDomain : PanacheEntityBase {
     companion object: PanacheCompanion<MyDomain> {
         fun findByName(name: String) = find("name", name).firstResult()
         fun all() = findAll()
     }
+
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    var id: Long? = null
 
     lateinit var name: String
 
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,4 +2,4 @@ insert into MyDomain values (1, 'test no 1');
 insert into MyDomain values (2, 'test no 2');
 insert into MyDomain values (3, 'test no 3');
 
-UPDATE MyDomain_SEQ SET next_val = 4;
+# UPDATE MyDomain_SEQ SET next_val = 4;
